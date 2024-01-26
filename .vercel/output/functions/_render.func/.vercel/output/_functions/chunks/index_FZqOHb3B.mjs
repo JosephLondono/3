96ff31052740cfb32caa1895836d23be6fbb0db2
JosephLondono <londonoj888@gmail.com>
2024-01-25 20:41:47 -0500

    a

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_FZqOHb3B.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_yPr_KJq6.mjs').then(n => n.i);
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/movie_6L7YNDbo.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/movie_joB1_ZVD.mjs');
+
+export { page };
